a71de7591b9322d13db243465f1ee856e181c0ef
Damius Green <40849814+Iforgedit@users.noreply.github.com>
2019-10-14 12:13:24 -0400

    Updates

---
+++ precache-manifest.68f1c97f41eae4ee8b16510e63c7baa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a96581ed2aa6326fd8a73773e2a356",
+    "url": "/bowls/index.html"
+  },
+  {
+    "revision": "33b9ad20fed78eee1c26",
+    "url": "/bowls/static/css/main.6587be9e.chunk.css"
+  },
+  {
+    "revision": "300a5debdc976e5bf713",
+    "url": "/bowls/static/js/2.25b1c4fe.chunk.js"
+  },
+  {
+    "revision": "33b9ad20fed78eee1c26",
+    "url": "/bowls/static/js/main.fd7cb724.chunk.js"
+  },
+  {
+    "revision": "6b4e7531e100c62aeffd",
+    "url": "/bowls/static/js/runtime~main.0af59f61.js"
+  }
+]);
